050e9f34128f8a74ead316030ece58c5f3fbf0d9
wantt <wantt2@qq.com>
2019-04-13 19:57:49 +0800

    add

---
+++ Makefile
@@ -0,0 +1,2 @@
+main:
+	gcc test.c -o main -lwiringPi -lpthread -lcrypt -lm -lrt
---
+++ test.c
@@ -0,0 +1,29 @@
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define COMPORT "/dev/ttyS0"
+#define COUNT 10
+int main(void)
+{
+	  int fd;
+	    if(wiringPiSetupSys() < 0) 
+		      {
+			            printf("init sys fail\n");
+				          return 1;
+					    }
+	      if((fd = serialOpen(COMPORT,9600)) < 0 ) 
+		        {
+				      printf("open serial fail\n");
+				            return 1;
+					      }
+	       int count = 0;
+	        while( count < COUNT)
+			 {
+				    serialPuts(fd,"123");
+				       sleep(1);
+				          count ++;
+					   }
+		serialClose(fd);
+}
